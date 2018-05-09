e19e7c97e8f14517b68c99a47c5491a32e2a5a1c
Felice Forby <feliceforby@gmail.com>
2018-05-09 04:23:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  # find anagram matches given an array of words
+  def match(words)
+  
+  end
+end
