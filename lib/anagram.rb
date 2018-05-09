d8ab11e7fd41a25beb0c242c3a7a0aef9d7f74df
Felice Forby <feliceforby@gmail.com>
2018-05-09 04:27:54 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   # find anagram matches given an array of words
   def match(possible_anagrams)
     possible_anagrams.collect do |possible_anagram|
-      self.word.split("").sort == possible_anagram.split("").sort
+      @word.split("").sort == possible_anagram.split("").sort
     end
   end
 end
